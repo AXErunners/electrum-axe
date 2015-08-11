8a9885d68215f386f2c5826f439ef24f7df947cb
Kefkius <kefkius@mail.com>
2015-08-11 13:31:59 -0400

    Default DASH servers

--- lib/network.py
+++ lib/network.py
@@ -18,18 +18,10 @@ from collections import deque
 DEFAULT_PORTS = {'t':'50001', 's':'50002', 'h':'8081', 'g':'8082'}
 
 DEFAULT_SERVERS = {
-    'electrum.be':{'t':'50001', 's':'50002'},
-    'electrum.drollette.com':{'t':'50001', 's':'50002'},
-    'erbium1.sytes.net':{'t':'50001', 's':'50002'},
-    'ecdsa.net':{'t':'50001', 's':'110'},
-    'electrum0.electricnewyear.net':{'t':'50001', 's':'50002'},
-    'kirsche.emzy.de':DEFAULT_PORTS,
-    'jwu42.hopto.org':{'t':'50001', 's':'50002'},
-    'VPS.hsmiths.com':{'t':'50001', 's':'50002'},
-    'ELECTRUM.jdubya.info':{'t':'50001', 's':'50002'},
-    'electrum.no-ip.org':{'t':'50001', 's':'50002', 'g':'443'},
-    'electrum.thwg.org':DEFAULT_PORTS,
-    'us.electrum.be':{'t':'50001', 's':'50002'},
+    'electrum.darkcointalk.org':DEFAULT_PORTS, # propulsion
+    'drk1.electrum-servers.us':DEFAULT_PORTS,  # elm4ever
+    'electrum.drk.siampm.com':DEFAULT_PORTS,   # thelazier
+    'electrum-drk.club':DEFAULT_PORTS,         # duffman
 }
 
 NODES_RETRY_INTERVAL = 60
