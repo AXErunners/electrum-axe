a9800b045f8556dc31b26be60abbb70dff2bc07c
Kefkius <kefkius@mail.com>
2015-08-11 13:12:14 -0400

    Rename user directory

--- lib/util.py
+++ lib/util.py
@@ -100,13 +100,13 @@ def profiler(func):
 
 def user_dir():
     if "HOME" in os.environ:
-        return os.path.join(os.environ["HOME"], ".electrum")
+        return os.path.join(os.environ["HOME"], ".electrum-dash")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "Electrum")
+        return os.path.join(os.environ["APPDATA"], "Electrum-DASH")
     elif "LOCALAPPDATA" in os.environ:
-        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum")
+        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum-DASH")
     elif 'ANDROID_DATA' in os.environ:
-        return "/sdcard/electrum/"
+        return "/sdcard/electrum-dash/"
     else:
         #raise Exception("No home directory found in environment variables.")
         return
