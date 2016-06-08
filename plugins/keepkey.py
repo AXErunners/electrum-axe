3ee68635ec52cedf2a1e901c3ccb5d789357e8fc
Holger Schinzel <holger.schinzel@gmx.de>
2016-06-08 20:01:17 +0200

    fix addrtype

--- plugins/keepkey.py
+++ plugins/keepkey.py
@@ -321,9 +321,9 @@ class Plugin(BasePlugin):
                 txoutputtype.address = address
             txoutputtype.amount = amount
             addrtype, hash_160 = bc_address_to_hash_160(address)
-            if addrtype == 0:
+            if addrtype == 76:
                 txoutputtype.script_type = types.PAYTOADDRESS
-            elif addrtype == 5:
+            elif addrtype == 16:
                 txoutputtype.script_type = types.PAYTOSCRIPTHASH
             else:
                 raise BaseException('addrtype')
