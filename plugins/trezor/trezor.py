1eddb4fccd6bd95a027e6b5fa3ca46cde5dbbcae
Holger Schinzel <holger.schinzel@gmx.de>
2016-07-16 17:54:47 +0200

    Revert "debugging"
    This reverts commit dde3296d57531530b73352318a42338fc3936254.

--- plugins/trezor/trezor.py
+++ plugins/trezor/trezor.py
@@ -11,9 +11,11 @@ class TrezorPlugin(TrezorCompatiblePlugin):
     libraries_URL = 'https://github.com/trezor/python-trezor'
     minimum_firmware = (1, 2, 1)
     wallet_class = TrezorWallet
-
-    from .client import TrezorClient as client_class
-    import trezorlib.ckd_public as ckd_public
-    from trezorlib.client import types
-    from trezorlib.transport_hid import HidTransport, DEVICE_IDS
-    libraries_available = True
+    try:
+        from .client import TrezorClient as client_class
+        import trezorlib.ckd_public as ckd_public
+        from trezorlib.client import types
+        from trezorlib.transport_hid import HidTransport, DEVICE_IDS
+        libraries_available = True
+    except ImportError:
+        libraries_available = False
