670720cdb7036bf76af31122f033e15e5391105d
-k <slowdive@me.com>
2018-03-12 18:41:06 -0400

    Update before_install-linux.sh

--- contrib/travis/before_install-linux.sh
+++ contrib/travis/before_install-linux.sh
@@ -6,7 +6,7 @@ fi
 
 cd build
 
-docker build -f Dockerfile-linux -t akhavr/electrum-axe-release:Linux .
+docker build -f Dockerfile-linux -t axerunners/electrum-axe-release:Linux .
 ./python-x11_hash-wine.sh
 ./python-trezor-wine.sh
-docker build -f Dockerfile-wine -t akhavr/electrum-axe-release:Wine .
+docker build -f Dockerfile-wine -t axerunners/electrum-axe-release:Wine .
