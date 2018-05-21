9d230a7716f55337fe9593174aadc30948a8bc81
-k <slowdive@me.com>
2018-05-21 19:53:10 -0400

    update linux script

--- contrib/travis/travis-build-linux.sh
+++ contrib/travis/travis-build-linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [[ $TRAVIS_PYTHON_VERSION != 3.4 ]]; then
   exit 0
@@ -30,6 +31,12 @@ docker run --rm \
     -v $(pwd)/electrum-axe/:$WINEPREFIX/drive_c/electrum-axe \
     -w /opt/electrum-axe \
     -t zebralucky/electrum-axe-winebuild:Wine /opt/build_wine.sh
+
+sudo chown -R 1000 electrum-axe
+docker run --rm \
+    -v $(pwd)/electrum-axe:/home/buildozer/build \
+    -t axerunners/electrum-axe-winebuild:Kivy bash -c \
+    './contrib/make_packages && mv ./contrib/packages . && ./contrib/make_apk'
 
 export WINEARCH=win64
 export WINEPREFIX=/root/.wine-64
