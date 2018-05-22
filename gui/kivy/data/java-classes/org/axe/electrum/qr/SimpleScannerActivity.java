d5e71abd3aa8ee41d0693a6fa60bb3a8534c68bd
-k <slowdive@me.com>
2018-05-21 21:23:17 -0400

    edit ssa java

--- gui/kivy/data/java-classes/org/axe/electrum/qr/SimpleScannerActivity.java
+++ gui/kivy/data/java-classes/org/axe/electrum/qr/SimpleScannerActivity.java
@@ -1,4 +1,4 @@
-package org.electrum.qr;
+package org.axe.electrum.qr;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -14,7 +14,7 @@ import com.google.zxing.BarcodeFormat;
 
 public class SimpleScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
     private ZXingScannerView mScannerView;
-    final String TAG = "org.electrum.SimpleScannerActivity";
+    final String TAG = "org.axe.electrum.SimpleScannerActivity";
 
     @Override
     public void onCreate(Bundle state) {
