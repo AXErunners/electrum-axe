8ababb5ef08efbf7a77843f11d742b428fd442b3
-k <slowdive@me.com>
2018-05-21 21:37:30 -0400

    update wiki link

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -542,7 +542,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         help_menu.addAction(_("&About"), self.show_about)
         help_menu.addAction(_("&Official website"), lambda: webbrowser.open("http://axerunners.com"))
         help_menu.addSeparator()
-        help_menu.addAction(_("&Documentation"), lambda: webbrowser.open("https://docs.axe.org/en/latest/wallets/index.html#axe-electrum-wallet")).setShortcut(QKeySequence.HelpContents)
+        help_menu.addAction(_("&Documentation"), lambda: webbrowser.open("https://github.com/AXErunners/axe/wiki")).setShortcut(QKeySequence.HelpContents)
         help_menu.addAction(_("&Report Bug"), self.show_report_bug)
         help_menu.addSeparator()
         help_menu.addAction(_("&Donate to server"), self.donate_to_server)
