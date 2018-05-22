c9002062a4b20af6b62d525d93e1c9bc809c27e9
-k <slowdive@me.com>
2018-05-22 00:48:46 -0400

    add apk

---
+++ contrib/travis/before_install-linux-apk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ev
+
+if [[ -z $TRAVIS_TAG ]]; then
+  echo TRAVIS_TAG unset, exiting
+  exit 1
+fi
+
+docker pull axerunners/electrum-axe-winebuild:Linux
+docker pull axerunners/electrum-axe-winebuild:Kivy
