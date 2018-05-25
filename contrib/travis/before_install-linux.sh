88b530e2ecc4d3544d489769e0f3ed637ab53623
-k <slowdive@me.com>
2018-05-25 14:43:15 -0400

    replace lib link

--- contrib/travis/before_install-linux.sh
+++ contrib/travis/before_install-linux.sh
@@ -6,5 +6,5 @@ if [[ -z $TRAVIS_TAG ]]; then
   exit 1
 fi
 
-docker pull zebralucky/electrum-axe-winebuild:Linux
-docker pull zebralucky/electrum-axe-winebuild:Wine
+docker pull axerunners/electrum-axe-winebuild:Linux
+docker pull axerunners/electrum-axe-winebuild:Wine
