01e6a02aa8a395e23afdac8ecf2a9fa0060278bc
zebra-lucky <zebra.lucky@gmail.com>
2018-06-09 00:30:43 +0300

    add headers init via checkpoints.json

--- lib/constants.py
+++ lib/constants.py
@@ -46,7 +46,7 @@ class BitcoinMainnet:
     GENESIS = "00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6"
     DEFAULT_PORTS = {'t': '50001', 's': '50002'}
     DEFAULT_SERVERS = read_json('servers.json', {})
-    CHECKPOINTS = []
+    CHECKPOINTS = read_json('checkpoints.json', [])
 
     XPRV_HEADERS = {
         'standard':    0x0488ade4,  # xprv
@@ -67,7 +67,7 @@ class BitcoinTestnet:
     GENESIS = "00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c"
     DEFAULT_PORTS = {'t': '51001', 's': '51002'}
     DEFAULT_SERVERS = read_json('servers_testnet.json', {})
-    CHECKPOINTS = []
+    CHECKPOINTS = read_json('checkpoints_testnet.json', [])
 
     XPRV_HEADERS = {
         'standard':    0x04358394,  # tprv
