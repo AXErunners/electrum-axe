1c10e0c2862a1ede0f69d5f35862249e9f4051d5
SomberNight <somber.night@protonmail.com>
2018-07-03 17:17:20 +0200

    network: disconnect from server on malformed header subs
    closes #4478

--- lib/network.py
+++ lib/network.py
@@ -1107,7 +1107,12 @@ class Network(util.DaemonThread):
             self.invalid_version(i.server)
 
     def on_notify_header(self, interface, header_dict):
-        header_hex, height = header_dict['hex'], header_dict['height']
+        try:
+            header_hex, height = header_dict['hex'], header_dict['height']
+        except KeyError:
+            # no point in keeping this connection without headers sub
+            self.connection_down(interface.server)
+            return
         header = blockchain.deserialize_header(util.bfh(header_hex), height)
         if height < self.max_checkpoint():
             self.connection_down(interface.server)
