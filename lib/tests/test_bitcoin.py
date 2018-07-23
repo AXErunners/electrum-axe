a30789b3689f17b4ca0855300054431274a65174
ddude <32743542+ddude1@users.noreply.github.com>
2018-07-23 13:22:08 -0400

    tcf3

--- lib/tests/test_bitcoin.py
+++ lib/tests/test_bitcoin.py
@@ -632,7 +632,7 @@ class Test_keyImport(SequentialTestCase):
            {'priv': 'SzavMBLoXU6kDrqtUVmffv',   #E9873D79C6D87DC0FB6A5778633389F4453213303DA61F20BD67FC233AA33262
             'exported_privkey': 'p2pkh:XK7aeZ9n1H1G4W4gkaf4PtqJUJPBcXDEdj9DXfrs2dH3gqJMtvHn',
             'pub': '02588d202afcc1ee4ab5254c7847ec25b9a135bbda0f2bc69ee1a714749fd77dc9',
-            'address': 'PGs65CKCQQ3KufnqHXRgLpmCK6nxTwLixp', # somehow needs uncompressed .. compressed value is PGs65CKCQQ3KufnqHXRgLpmCK6nxTwLixp
+            'address': 'PKnDg15k847HvN9GhjzMatLQuuqLYyMQbJ', # somehow needs uncompressed .. compressed value is PGs65CKCQQ3KufnqHXRgLpmCK6nxTwLixp
             'minikey': True,
             'txin_type': 'p2pkh',
             'compressed': False,  # this is actually ambiguous... issue #2748
