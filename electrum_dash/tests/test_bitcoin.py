197a4d15f8e4304aa6a6c4fd1ec652f4df671819
zebra-lucky <zebra.lucky@gmail.com>
2018-08-17 13:54:02 +0300

    move TrezorClient.expand_path to bitcoin.py
    and allow its input to end with a '/' slash

--- electrum_dash/tests/test_bitcoin.py
+++ electrum_dash/tests/test_bitcoin.py
@@ -11,7 +11,7 @@ from electrum_dash.bitcoin import (
     deserialize_privkey, serialize_privkey,
     is_b58_address, address_to_scripthash, is_minikey, is_compressed, is_xpub,
     xpub_type, is_xprv, is_bip32_derivation, seed_type, EncodeBase58Check,
-    script_num_to_hex, push_script, add_number_to_script, int_to_hex,
+    script_num_to_hex, push_script, add_number_to_script, int_to_hex, convert_bip32_path_to_list_of_uint32
     deserialize_xprv, deserialize_xpub, deserialize_drkv, deserialize_drkp)
 from electrum_dash import ecc, crypto, constants
 from electrum_dash.ecc import number_to_string, string_to_number
@@ -452,6 +452,11 @@ class Test_xprv_xpub(SequentialTestCase):
         self.assertFalse(is_bip32_derivation("n/"))
         self.assertFalse(is_bip32_derivation(""))
         self.assertFalse(is_bip32_derivation("m/q8462"))
+
+    def test_convert_bip32_path_to_list_of_uint32(self):
+        self.assertEqual([0, 0x80000001, 0x80000001], convert_bip32_path_to_list_of_uint32("m/0/-1/1'"))
+        self.assertEqual([], convert_bip32_path_to_list_of_uint32("m/"))
+        self.assertEqual([2147483692, 2147488889, 221], convert_bip32_path_to_list_of_uint32("m/44'/5241'/221"))
 
     def test_xtype_from_derivation(self):
         self.assertEqual('standard', xtype_from_derivation("m/44'"))
