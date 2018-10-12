b138fe487c5777901a0eea5420c4175f570058c5
zebra-lucky <zebra.lucky@gmail.com>
2018-10-12 15:09:24 +0300

    rename release name/body during signing process

--- contrib/sign-releases.py
+++ contrib/sign-releases.py
@@ -113,7 +113,8 @@ try:
     import colorama
     from colorama import Fore, Style
     from github_release import (get_releases, gh_asset_download,
-                                gh_asset_upload, gh_asset_delete)
+                                gh_asset_upload, gh_asset_delete,
+                                gh_release_edit)
     from urllib3 import PoolManager
 except ImportError as e:
     print('Import error:', e)
@@ -511,7 +512,7 @@ class SignApp(object):
                             dry_run=self.dry_run)
 
             if sdist_match and is_newest_release:
-                self.make_ppa(sdist_match, tmpdir)
+                self.make_ppa(sdist_match, tmpdir, tag)
 
     def sign_apk(self, unsigned_name, version):
         """Sign unsigned release apk"""
@@ -533,8 +534,10 @@ class SignApp(object):
 
         return name
 
-    def make_ppa(self, sdist_match, tmpdir):
+    def make_ppa(self, sdist_match, tmpdir, tag):
         """Build, sign and upload dsc to launchpad.net ppa from sdist.tar.gz"""
+        repo = self.repo
+
         with ChdirTemporaryDirectory() as ppa_tmpdir:
             sdist_name = sdist_match.group(0)
             version = sdist_match.group(1)
@@ -573,6 +576,10 @@ class SignApp(object):
                     changes = '\n'.join(changes)
                 else:
                     changes = '\n  * Porting to ppa\n\n'
+
+            if not self.dry_run:
+                gh_release_edit(repo, tag, name=version)
+                gh_release_edit(repo, tag, body=changes)
 
             os.chdir(sdist_dir)
             print('  Making PPAs for series: %s' % (', '.join(series)))
