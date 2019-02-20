a6de8d3105617e05208bef2a909f59462fc1c6b3
-k <slowdive@me.com>
2019-02-19 17:42:21 -0800

    rename

--- electrum_axe/gui/kivy/uix/dialogs/settings.py
+++ electrum_axe/gui/kivy/uix/dialogs/settings.py
@@ -17,7 +17,7 @@ Builder.load_string('''
 
 <SettingsDialog@Popup>
     id: settings
-    title: _('AXE Electrum Settings')
+    title: _('Axe Electrum Settings')
     disable_pin: False
     use_encryption: False
     BoxLayout:
@@ -44,7 +44,7 @@ Builder.load_string('''
                 SettingsItem:
                     bu: app.base_unit
                     title: _('Denomination') + ': ' + self.bu
-                    description: _("Base unit for AXE amounts.")
+                    description: _("Base unit for Axe amounts.")
                     action: partial(root.unit_dialog, self)
                 CardSeparator
                 SettingsItem:
