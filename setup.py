a1dff8bcc92e7c8c16e605922e5ee1ab8d51df2e
-k <slowdive@me.com>
2019-03-02 11:34:15 -0800

    add plugins root

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
         'electrum_axe',
         'electrum_axe.gui',
         'electrum_axe.gui.qt',
+        'electrum_axe.plugins',
     ] + [('electrum_axe.plugins.'+pkg) for pkg in find_packages('electrum_axe/plugins')],
     package_dir={
         'electrum_axe': 'electrum_axe'
