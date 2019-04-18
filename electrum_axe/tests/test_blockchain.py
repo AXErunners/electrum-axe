b92287dd0e06a3d087dcfcfc8289bee024e43ba4
-k <slowdive@me.com>
2019-04-17 17:39:18 -0700

    Create test_blockchain.py

---
+++ electrum_axe/tests/test_blockchain.py
@@ -0,0 +1,239 @@
+import shutil
+import tempfile
+import os
+
+from electrum_axe import constants, blockchain
+from electrum_axe.simple_config import SimpleConfig
+from electrum_axe.blockchain import Blockchain, deserialize_header, hash_header
+from electrum_axe.util import bh2u, bfh, make_dir
+
+from . import SequentialTestCase
+
+
+class TestBlockchain(SequentialTestCase):
+
+    HEADERS = {
+        'A': deserialize_header(bfh("010000000000000000000000000000000000000000000000000000000000000000000000c762a6567f3cc092f0684bb62b7e00a84890b990f07cc71a6bb58d64b98e02e0 b9968054 ffff7f20 ffba1000"), 0),
+        'B': deserialize_header(bfh("000000202e3df23eec5cd6a86edd509539028e2c3a3dc05315eb28f2baa43218ca080000186c8dfd970a4545f79916bc1d75c9d00432f57c89209bf3bb115b7612848f509c25f45bffff7f2000000000"), 1),
+        'C': deserialize_header(bfh("000000200a8be74779a59fec4f56abd6ce33bf2a8a1e896b0290a2aba90cf8fa6e6a88f7bf2cbf153013a1c54abaf70e95198fcef2f3059cc6b4d0f7e876808e7d24d11cc825f45bffff7f2000000000"), 2),
+        'D': deserialize_header(bfh("000000204a030521422dda1f980cfc2b38149edd3d8eab547e6efa3ab855048feb68dbdae71019d7feecd9b8596eca9a67032c5f4641b23b5d731dc393e37de7f9c2f299e725f45bffff7f2000000000"), 3),
+        'E': deserialize_header(bfh("00000020e39959c005b364248b24a17a72fcfe89d8478c71645b85edd444031ef5e5f896a3586da94c71753f27c075f57f44faf913c31177a0957bbda42e7699e3a2141aed25f45bffff7f2001000000"), 4),
+        'F': deserialize_header(bfh("00000020d02b1711b7bc72feb7b3e599e9f9bb67f163c95203a64f6dcd4f6176c15d31437aee1d692d1615c3bdf52c291032144ce9e3b258a473c17c745047f3431ff8e2ee25f45bffff7f2000000000"), 5),
+        'O': deserialize_header(bfh("00000020ed0bfee047765d7f4233106a13b4ff6d6c67f7ef9aec0e7466759f00ea74b2613a141ce635cbb1cd2b3a4fcdd0a3380517845ba41736c82a79cab535d31128066526f45bffff7f2001000000"), 6),
+        'P': deserialize_header(bfh("000000201f9b9f1e295fd4eda90b03b62a676f93642d28c258d8222a2e9d5f0c75cae0a99690c2fe7c1a4450c74dc908fe94dd96c3b0637d51475e9e06a78e944a0c7fe28126f45bffff7f2000000000"), 7),
+        'Q': deserialize_header(bfh("000000200076268f577977b9e7386f68a9c3c332aa613d27243abd8167a1bd891adf404f148be228a4c3f2061bafe7efdfc4a8d5a94759464b9b5c619994d45dfcaf49e1a126f45bffff7f2000000000"), 8),
+        'R': deserialize_header(bfh("000000208cfac7d4caa975c6b7fe770a8ea35a77a02f6e9b1900bae67a389619095c757515681cb2d00ff889193f6a68a93f5096aeb2d84ca0af6185a462555822552221a626f45bffff7f2000000000"), 9),
+        'S': deserialize_header(bfh("00000020936defed88e60da5cef2106338ef9ec221d65e9226f1fc29ec76e4b7c34a649c9dc087fc977b06c24a69c682d1afd1020e6dc1f087571ccec66310a786e1548fab26f45bffff7f2000000000"), 10),
+        'T': deserialize_header(bfh("00000020372528176ba7c014b6f388ba338c7a87a5c50bc4d8a1a1d5900cbf5725e6822903b243756c25053253aeda309604363460a3911015929e68705bd89dff6fe064b026f45bffff7f2002000000"), 11),
+        'U': deserialize_header(bfh("00000020c5a999182175cb571c7a15a08b8577e21b67c156a2c0ceebcce0d897e664fc3ad67cb902a7d807cee7676cb543feec3e053aa824d5dfb528d5b94f9760313d9db726f45bffff7f2001000000"), 12),
+        'G': deserialize_header(bfh("00000020ed0bfee047765d7f4233106a13b4ff6d6c67f7ef9aec0e7466759f00ea74b2613a141ce635cbb1cd2b3a4fcdd0a3380517845ba41736c82a79cab535d31128066928f45bffff7f2001000000"), 6),
+        'H': deserialize_header(bfh("00000020f8ca2216e002361e7cc1dd3e1197443e0b8068adaeec43d14be0e4f2159659e39690c2fe7c1a4450c74dc908fe94dd96c3b0637d51475e9e06a78e944a0c7fe26a28f45bffff7f2002000000"), 7),
+        'I': deserialize_header(bfh("00000020996b8b880bfe34b81dda59ae28ee28625a4dff565f671540a4703ebabd0ab991148be228a4c3f2061bafe7efdfc4a8d5a94759464b9b5c619994d45dfcaf49e16a28f45bffff7f2000000000"), 8),
+        'J': deserialize_header(bfh("000000201d5a4dfeeda94c6e4c3e40ce5c30df07e8103dba70cbce9d6b0890405c76b06715681cb2d00ff889193f6a68a93f5096aeb2d84ca0af6185a462555822552221c928f45bffff7f2000000000"), 9),
+        'K': deserialize_header(bfh("00000020f93c46944a529187faae721951e66e187a0e910104e91ec8d1d4a914cadd79a89dc087fc977b06c24a69c682d1afd1020e6dc1f087571ccec66310a786e1548fca28f45bffff7f2000000000"), 10),
+        'L': deserialize_header(bfh("00000020d76bdf59ed1ce4a4a31aa7649f8a39da2b956515f3bdb78b2bcdaaed60444bad03b243756c25053253aeda309604363460a3911015929e68705bd89dff6fe064ca28f45bffff7f2000000000"), 11),
+        'M': deserialize_header(bfh("000000201d5a4dfeeda94c6e4c3e40ce5c30df07e8103dba70cbce9d6b0890405c76b06715681cb2d00ff889193f6a68a93f5096aeb2d84ca0af6185a4625558225522214229f45bffff7f2000000000"), 9),
+        'N': deserialize_header(bfh("00000020ff8ef64ad77c7c02103127be41dc39dda5f4dd17cbbaa7475fa8b7a3dd110ee19dc087fc977b06c24a69c682d1afd1020e6dc1f087571ccec66310a786e1548f4329f45bffff7f2003000000"), 10),
+        'X': deserialize_header(bfh("000000202857b96792f630a80f7c834afd5985b833794037930c1fe655c23b6eb769c85203b243756c25053253aeda309604363460a3911015929e68705bd89dff6fe0649b29f45bffff7f2002000000"), 11),
+        'Y': deserialize_header(bfh("000000206cc9a0dec93cffaab358ef9bd06fa0137d53e37a4b251f57da831ef31fccf9f2d67cb902a7d807cee7676cb543feec3e053aa824d5dfb528d5b94f9760313d9d9b29f45bffff7f2000000000"), 12),
+        'Z': deserialize_header(bfh("00000020756a6bfe58694141de4abf3317bccfa105b5ec30b997dda15a9ab02a9d86eba00f2596c29203f8a0f71ae94193092dc8f113be3dbee4579f1e649fa3d6dcc38c622ef45bffff7f2003000000"), 13),
+    }
+    # tree of headers:
+    #                                            - M <- N <- X <- Y <- Z
+    #                                          /
+    #                             - G <- H <- I <- J <- K <- L
+    #                           /
+    # A <- B <- C <- D <- E <- F <- O <- P <- Q <- R <- S <- T <- U
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        constants.set_regtest()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        constants.set_mainnet()
+
+    def setUp(self):
+        super().setUp()
+        self.data_dir = tempfile.mkdtemp()
+        make_dir(os.path.join(self.data_dir, 'forks'))
+        self.config = SimpleConfig({'electrum_path': self.data_dir})
+        blockchain.blockchains = {}
+
+    def tearDown(self):
+        super().tearDown()
+        shutil.rmtree(self.data_dir)
+
+    def _append_header(self, chain: Blockchain, header: dict):
+        self.assertTrue(chain.can_connect(header))
+        chain.save_header(header)
+
+    def test_forking_and_swapping(self):
+        blockchain.blockchains[constants.net.GENESIS] = chain_u = Blockchain(
+            config=self.config, forkpoint=0, parent=None,
+            forkpoint_hash=constants.net.GENESIS, prev_hash=None)
+        open(chain_u.path(), 'w+').close()
+
+        self._append_header(chain_u, self.HEADERS['A'])
+        self._append_header(chain_u, self.HEADERS['B'])
+        self._append_header(chain_u, self.HEADERS['C'])
+        self._append_header(chain_u, self.HEADERS['D'])
+        self._append_header(chain_u, self.HEADERS['E'])
+        self._append_header(chain_u, self.HEADERS['F'])
+        self._append_header(chain_u, self.HEADERS['O'])
+        self._append_header(chain_u, self.HEADERS['P'])
+        self._append_header(chain_u, self.HEADERS['Q'])
+        self._append_header(chain_u, self.HEADERS['R'])
+
+        chain_l = chain_u.fork(self.HEADERS['G'])
+        self._append_header(chain_l, self.HEADERS['H'])
+        self._append_header(chain_l, self.HEADERS['I'])
+        self._append_header(chain_l, self.HEADERS['J'])
+
+        # do checks
+        self.assertEqual(2, len(blockchain.blockchains))
+        self.assertEqual(1, len(os.listdir(os.path.join(self.data_dir, "forks"))))
+        self.assertEqual(0, chain_u.forkpoint)
+        self.assertEqual(None, chain_u.parent)
+        self.assertEqual(constants.net.GENESIS, chain_u._forkpoint_hash)
+        self.assertEqual(None, chain_u._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "blockchain_headers"), chain_u.path())
+        self.assertEqual(10 * 80, os.stat(chain_u.path()).st_size)
+        self.assertEqual(6, chain_l.forkpoint)
+        self.assertEqual(chain_u, chain_l.parent)
+        self.assertEqual(hash_header(self.HEADERS['G']), chain_l._forkpoint_hash)
+        self.assertEqual(hash_header(self.HEADERS['F']), chain_l._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "forks", "fork2_6_61b274ea009f7566740eec9aeff7676c6dffb4136a1033427f5d7647e0fe0bed_e3599615f2e4e04bd143ecaead68800b3e4497113eddc17c1e3602e01622caf8"), chain_l.path())
+        self.assertEqual(4 * 80, os.stat(chain_l.path()).st_size)
+
+        self._append_header(chain_l, self.HEADERS['K'])
+
+        # chains were swapped, do checks
+        self.assertEqual(2, len(blockchain.blockchains))
+        self.assertEqual(1, len(os.listdir(os.path.join(self.data_dir, "forks"))))
+        self.assertEqual(6, chain_u.forkpoint)
+        self.assertEqual(chain_l, chain_u.parent)
+        self.assertEqual(hash_header(self.HEADERS['O']), chain_u._forkpoint_hash)
+        self.assertEqual(hash_header(self.HEADERS['F']), chain_u._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "forks", "fork2_6_61b274ea009f7566740eec9aeff7676c6dffb4136a1033427f5d7647e0fe0bed_a9e0ca750c5f9d2e2a22d858c2282d64936f672ab6030ba9edd45f291e9f9b1f"), chain_u.path())
+        self.assertEqual(4 * 80, os.stat(chain_u.path()).st_size)
+        self.assertEqual(0, chain_l.forkpoint)
+        self.assertEqual(None, chain_l.parent)
+        self.assertEqual(constants.net.GENESIS, chain_l._forkpoint_hash)
+        self.assertEqual(None, chain_l._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "blockchain_headers"), chain_l.path())
+        self.assertEqual(11 * 80, os.stat(chain_l.path()).st_size)
+        for b in (chain_u, chain_l):
+            self.assertTrue(all([b.can_connect(b.read_header(i), False) for i in range(b.height())]))
+
+        self._append_header(chain_u, self.HEADERS['S'])
+        self._append_header(chain_u, self.HEADERS['T'])
+        self._append_header(chain_u, self.HEADERS['U'])
+        self._append_header(chain_l, self.HEADERS['L'])
+
+        chain_z = chain_l.fork(self.HEADERS['M'])
+        self._append_header(chain_z, self.HEADERS['N'])
+        self._append_header(chain_z, self.HEADERS['X'])
+        self._append_header(chain_z, self.HEADERS['Y'])
+        self._append_header(chain_z, self.HEADERS['Z'])
+
+        # chain_z became best chain, do checks
+        self.assertEqual(3, len(blockchain.blockchains))
+        self.assertEqual(2, len(os.listdir(os.path.join(self.data_dir, "forks"))))
+        self.assertEqual(0, chain_z.forkpoint)
+        self.assertEqual(None, chain_z.parent)
+        self.assertEqual(constants.net.GENESIS, chain_z._forkpoint_hash)
+        self.assertEqual(None, chain_z._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "blockchain_headers"), chain_z.path())
+        self.assertEqual(14 * 80, os.stat(chain_z.path()).st_size)
+        self.assertEqual(9, chain_l.forkpoint)
+        self.assertEqual(chain_z, chain_l.parent)
+        self.assertEqual(hash_header(self.HEADERS['J']), chain_l._forkpoint_hash)
+        self.assertEqual(hash_header(self.HEADERS['I']), chain_l._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "forks", "fork2_9_67b0765c4090086b9dcecb70ba3d10e807df305cce403e4c6e4ca9edfe4d5a1d_a879ddca14a9d4d1c81ee90401910e7a186ee6511972aefa8791524a94463cf9"), chain_l.path())
+        self.assertEqual(3 * 80, os.stat(chain_l.path()).st_size)
+        self.assertEqual(6, chain_u.forkpoint)
+        self.assertEqual(chain_z, chain_u.parent)
+        self.assertEqual(hash_header(self.HEADERS['O']), chain_u._forkpoint_hash)
+        self.assertEqual(hash_header(self.HEADERS['F']), chain_u._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "forks", "fork2_6_61b274ea009f7566740eec9aeff7676c6dffb4136a1033427f5d7647e0fe0bed_a9e0ca750c5f9d2e2a22d858c2282d64936f672ab6030ba9edd45f291e9f9b1f"), chain_u.path())
+        self.assertEqual(7 * 80, os.stat(chain_u.path()).st_size)
+        for b in (chain_u, chain_l, chain_z):
+            self.assertTrue(all([b.can_connect(b.read_header(i), False) for i in range(b.height())]))
+
+        self.assertEqual(constants.net.GENESIS, chain_z.get_hash(0))
+        self.assertEqual(hash_header(self.HEADERS['F']), chain_z.get_hash(5))
+        self.assertEqual(hash_header(self.HEADERS['G']), chain_z.get_hash(6))
+        self.assertEqual(hash_header(self.HEADERS['I']), chain_z.get_hash(8))
+        self.assertEqual(hash_header(self.HEADERS['M']), chain_z.get_hash(9))
+        self.assertEqual(hash_header(self.HEADERS['Z']), chain_z.get_hash(13))
+
+    def test_doing_multiple_swaps_after_single_new_header(self):
+        blockchain.blockchains[constants.net.GENESIS] = chain_u = Blockchain(
+            config=self.config, forkpoint=0, parent=None,
+            forkpoint_hash=constants.net.GENESIS, prev_hash=None)
+        open(chain_u.path(), 'w+').close()
+
+        self._append_header(chain_u, self.HEADERS['A'])
+        self._append_header(chain_u, self.HEADERS['B'])
+        self._append_header(chain_u, self.HEADERS['C'])
+        self._append_header(chain_u, self.HEADERS['D'])
+        self._append_header(chain_u, self.HEADERS['E'])
+        self._append_header(chain_u, self.HEADERS['F'])
+        self._append_header(chain_u, self.HEADERS['O'])
+        self._append_header(chain_u, self.HEADERS['P'])
+        self._append_header(chain_u, self.HEADERS['Q'])
+        self._append_header(chain_u, self.HEADERS['R'])
+        self._append_header(chain_u, self.HEADERS['S'])
+
+        self.assertEqual(1, len(blockchain.blockchains))
+        self.assertEqual(0, len(os.listdir(os.path.join(self.data_dir, "forks"))))
+
+        chain_l = chain_u.fork(self.HEADERS['G'])
+        self._append_header(chain_l, self.HEADERS['H'])
+        self._append_header(chain_l, self.HEADERS['I'])
+        self._append_header(chain_l, self.HEADERS['J'])
+        self._append_header(chain_l, self.HEADERS['K'])
+        # now chain_u is best chain, but it's tied with chain_l
+
+        self.assertEqual(2, len(blockchain.blockchains))
+        self.assertEqual(1, len(os.listdir(os.path.join(self.data_dir, "forks"))))
+
+        chain_z = chain_l.fork(self.HEADERS['M'])
+        self._append_header(chain_z, self.HEADERS['N'])
+        self._append_header(chain_z, self.HEADERS['X'])
+
+        self.assertEqual(3, len(blockchain.blockchains))
+        self.assertEqual(2, len(os.listdir(os.path.join(self.data_dir, "forks"))))
+
+        # chain_z became best chain, do checks
+        self.assertEqual(0, chain_z.forkpoint)
+        self.assertEqual(None, chain_z.parent)
+        self.assertEqual(constants.net.GENESIS, chain_z._forkpoint_hash)
+        self.assertEqual(None, chain_z._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "blockchain_headers"), chain_z.path())
+        self.assertEqual(12 * 80, os.stat(chain_z.path()).st_size)
+        self.assertEqual(9, chain_l.forkpoint)
+        self.assertEqual(chain_z, chain_l.parent)
+        self.assertEqual(hash_header(self.HEADERS['J']), chain_l._forkpoint_hash)
+        self.assertEqual(hash_header(self.HEADERS['I']), chain_l._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "forks", "fork2_9_67b0765c4090086b9dcecb70ba3d10e807df305cce403e4c6e4ca9edfe4d5a1d_a879ddca14a9d4d1c81ee90401910e7a186ee6511972aefa8791524a94463cf9"), chain_l.path())
+        self.assertEqual(2 * 80, os.stat(chain_l.path()).st_size)
+        self.assertEqual(6, chain_u.forkpoint)
+        self.assertEqual(chain_z, chain_u.parent)
+        self.assertEqual(hash_header(self.HEADERS['O']), chain_u._forkpoint_hash)
+        self.assertEqual(hash_header(self.HEADERS['F']), chain_u._prev_hash)
+        self.assertEqual(os.path.join(self.data_dir, "forks", "fork2_6_61b274ea009f7566740eec9aeff7676c6dffb4136a1033427f5d7647e0fe0bed_a9e0ca750c5f9d2e2a22d858c2282d64936f672ab6030ba9edd45f291e9f9b1f"), chain_u.path())
+        self.assertEqual(5 * 80, os.stat(chain_u.path()).st_size)
+
+        self.assertEqual(constants.net.GENESIS, chain_z.get_hash(0))
+        self.assertEqual(hash_header(self.HEADERS['F']), chain_z.get_hash(5))
+        self.assertEqual(hash_header(self.HEADERS['G']), chain_z.get_hash(6))
+        self.assertEqual(hash_header(self.HEADERS['I']), chain_z.get_hash(8))
+        self.assertEqual(hash_header(self.HEADERS['M']), chain_z.get_hash(9))
+        self.assertEqual(hash_header(self.HEADERS['X']), chain_z.get_hash(11))
+
+        for b in (chain_u, chain_l, chain_z):
+            self.assertTrue(all([b.can_connect(b.read_header(i), False) for i in range(b.height())]))
